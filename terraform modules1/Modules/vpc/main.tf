ef2cc0afa3f66aeb83c6ef832e4ca27a10e22d16
juwonmichael <juanmichael179@gmail.com>
2025-02-19 18:25:21 +0000

    eks cluster fix

--- terraform modules1/Modules/vpc/main.tf
+++ terraform modules1/Modules/vpc/main.tf
@@ -48,7 +48,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_eip" "nat" {
   count = length(var.public_subnet_cidrs)
-  domain = "vpc"
+
 
   tags = {
     Name = "${var.cluster_name}-nat-${count.index + 1}"
---
+++ terraform modules1/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+    
+  }
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformstatebucket1279"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraformlocktable1279"
+    
+  }
+}
+
+ 
+module "vpc" {  
+  source = "./Modules/vpc"
+  cluster_name = var.cluster_name
+  vpc_cidr = var.vpc_cidr
+  availability_zones = var.availability_zones
+  private_subnet_cidrs = var.private_subnet_cidrs
+  public_subnet_cidrs = var.public_subnet_cidrs
+
+
+  
+}
+
+module "eks" {
+  source = "./Modules/eks"
+  cluster_name = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+  node_groups = var.node_groups
+
+}
