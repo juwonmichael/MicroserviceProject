b032c6bef72934e9dfcb0745917aa67850be9da9
juwonmichael <juanmichael179@gmail.com>
2025-02-19 15:13:38 +0000

    module completed

--- terraform modules1/Modules/vpc/output.tf
+++ terraform modules1/Modules/vpc/output.tf
@@ -1,25 +1,14 @@
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
 
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
 }
 
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-}
-
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
